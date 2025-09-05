e8cf902b21ae1546995aa6952d6858b063da3b11
Nathan Feliphe <nathanfcrocha@gmail.com>
2025-09-05 07:48:34 -0300

    feat: create the new metrics method, improve the redirecionar and cadastrar methods.

--- nathanflp/src/main/java/br/com/encurtadordeurl/nathanflp/service/LinkParaEncurtarService.java
+++ nathanflp/src/main/java/br/com/encurtadordeurl/nathanflp/service/LinkParaEncurtarService.java
@@ -1,43 +1,58 @@
 package br.com.encurtadordeurl.nathanflp.service;
 
-import br.com.encurtadordeurl.nathanflp.controller.dto.linkEncurtadoRequest;
-import br.com.encurtadordeurl.nathanflp.models.linkParaEncurtar;
-import br.com.encurtadordeurl.nathanflp.repository.linkParaEncurtarRepository;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Service;
+import br.com.encurtadordeurl.nathanflp.controller.dto.*;
+import br.com.encurtadordeurl.nathanflp.models.*;
+import br.com.encurtadordeurl.nathanflp.repository.*;
+import br.com.encurtadordeurl.nathanflp.utils.*;
+import org.apache.commons.lang3.*;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.stereotype.*;
 
-import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.Optional;
+import java.net.*;
+import java.time.*;
 
 @Service
-public record linkParaEncurtarService(linkParaEncurtarRepository repository) {
+public class LinkParaEncurtarService {
 
+    private final linkParaEncurtarRepository repository;
+    @Value("${link.expiration.days}")
+    private Integer expirationTime;
+    private final Logger log = LoggerFactory.getLogger(LinkParaEncurtarService.class);
 
-    public String cadastraLink(linkEncurtadoRequest request){
+    @Autowired
+    public LinkParaEncurtarService(linkParaEncurtarRepository repository) {
+        this.repository = repository;
+    }
 
-        String id;
+    public String cadastraLink(LinkEncurtadoRequest request, String baseUrl){
+        String redirectedUrlId;
 
+        log.info("Iniciando a geracao de id");
         do {
-            id = RandomStringUtils.randomAlphanumeric(5, 10);
-        } while (doesLinkAlreadyExist(id));
+            redirectedUrlId = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (doesLinkAlreadyExist(redirectedUrlId));
 
-        repository.save(new linkParaEncurtar(id, request.url(), LocalDateTime.now().plusDays(3)));
+        log.info("Id gerado: " + redirectedUrlId + ". Salvando no banco."  );
+        repository.save(new LinkParaEncurtar(redirectedUrlId, request.url(), LocalDateTime.now().plusDays(expirationTime)));
 
-        return id;
+        return baseUrl.replace("encurtar-url", redirectedUrlId);
     }
 
     public HttpHeaders redirecionaURL(String id ){
-
-        Optional<linkParaEncurtar> url = repository.findById(id);
-
-        if (url.isEmpty()) {
-            return null;
-        }
+        log.info("Iniciando redirecionamento de id: " + id);
+        LinkParaEncurtar url = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Link informado não existe nos nossos registros."));
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create(url.get().getFullUrl()));
+        headers.setLocation(URI.create(url.getFullUrl()));
+
+        log.info("Incrementando contagem de click e ultima vez clicada para id: " + url.getId());
+        url.increaseClickCount();
+        url.updateLastTimeClicked();
+
+        repository.save(url);
 
         return headers;
     }
@@ -47,12 +62,13 @@ public record linkParaEncurtarService(linkParaEncurtarRepository repository) {
     }
 
     public LinkMetricsResponse metricsResponse(String id){
-        log.info("Buscando url por id: " + id);
-        LinkParaEncurtar link = repository.findById(id).orElseThrow(() -> new RuntimeException("Link informado não existe nos nossos registros."));
-        log.info("Url encontrada");
-        log.info("Gerando novo linkMetricsReponse para id: " + link.getId());
+        log.info("Iniciando coleta de metrica para id: " + id);
+        LinkParaEncurtar link = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Link informado não existe nos nossos registros."));
 
-        return new LinkMetricsResponse(link.getId(),
+        log.info("Gerando novo linkMetricsResponse para id: " + link.getId());
+        return new LinkMetricsResponse(
+                link.getId(),
                 link.getClickCount(),
                 DateTimeFormatterUtil.format(link.getExpiresAt()));
     }
