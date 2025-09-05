4e9e39ae1821fdd2577ddf9620276ab84ad116c7
Nathan Feliphe <nathanfcrocha@gmail.com>
2025-09-05 07:37:57 -0300

    refactor: change repository entity name to match

--- nathanflp/src/main/java/br/com/encurtadordeurl/nathanflp/repository/linkParaEncurtarRepository.java
+++ nathanflp/src/main/java/br/com/encurtadordeurl/nathanflp/repository/linkParaEncurtarRepository.java
@@ -1,10 +1,10 @@
 package br.com.encurtadordeurl.nathanflp.repository;
 
-import br.com.encurtadordeurl.nathanflp.models.linkParaEncurtar;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import br.com.encurtadordeurl.nathanflp.models.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
 @Repository
-public interface linkParaEncurtarRepository extends JpaRepository<linkParaEncurtar,String> {
+public interface linkParaEncurtarRepository extends JpaRepository<LinkParaEncurtar,String> {
 
 }
