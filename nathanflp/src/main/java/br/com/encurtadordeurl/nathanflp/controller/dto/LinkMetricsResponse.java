a1806d112a7ab09c50fc878dd55e773d7ce0d55d
Nathan Feliphe <nathanfcrocha@gmail.com>
2025-09-04 21:55:03 -0300

    create new endpoint for metrics

---
+++ nathanflp/src/main/java/br/com/encurtadordeurl/nathanflp/controller/dto/LinkMetricsResponse.java
@@ -0,0 +1,8 @@
+package br.com.encurtadordeurl.nathanflp.controller.dto;
+
+public record LinkMetricsResponse(String url,
+                                  Integer clickCount,
+                                  String lastTimeClicked
+                                                                ) {
+
+}
